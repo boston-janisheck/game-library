35ae9b96f379a50945dae5bae7f31a180c70e1aa
Boston Janisheck <bostonjanisheck@gmail.com>
2024-08-16 15:30:54 -0500

    created navbar and footer

---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>&copy; 2024 /boston-janisheck</div>
+      <div>
+        <a href="https://github.com/boston-janisheck" className="footer-link">
+          GitHub
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/boston-janisheck/"
+          className="footer-link"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">SLOTS</div>
+      <a href="http://localhost:5000" className="navbar-link">
+        Play More Games
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;

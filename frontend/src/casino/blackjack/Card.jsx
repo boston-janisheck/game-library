f40fd62c799bba1ef7de26615ff5642daf290911
Boston Janisheck <bostonjanisheck@gmail.com>
2024-09-28 13:10:40 -0500

    cards styling

--- frontend/src/casino/blackjack/Card.jsx
+++ frontend/src/casino/blackjack/Card.jsx
@@ -9,14 +9,14 @@ const suitsSymbols = {
 
 const Card = ({ value, suit }) => {
   return (
-    <div className="card">
+    <div className={`card ${value === "Face Down" ? "face-down" : ""}`}>
       {value !== "Face Down" ? (
         <>
           <span className="card-value">{value}</span>
           <span className="card-suit">{suitsSymbols[suit]}</span>
         </>
       ) : (
-        <span className="card-value">{value}</span>
+        <div className="face-down-inner"></div>
       )}
     </div>
   );

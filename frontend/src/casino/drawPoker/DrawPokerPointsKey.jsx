d8a5c0879bc2c167b813a20daf01caf6705ad7f9
Boston Janisheck <bostonjanisheck@gmail.com>
2024-10-05 20:18:02 -0500

    DrawPokerPointsKey styling

--- frontend/src/casino/drawPoker/DrawPokerPointsKey.jsx
+++ frontend/src/casino/drawPoker/DrawPokerPointsKey.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import buxIcon from "../../shared/assets/images/bux.png";
 
 const DrawPokerPointsKey = () => {
+  const points = [
+    { title: "Royal Flush", value: 5000 },
+    { title: "Straight Flush", value: 1500 },
+    { title: "Four Of A Kind", value: 600 },
+    { title: "Full House", value: 300 },
+    { title: "Flush", value: 200 },
+    { title: "Straight", value: 125 },
+    { title: "Three Of A Kind", value: 75 },
+    { title: "Two Pair", value: 40 },
+    { title: "Jacks Or Better", value: 10 },
+  ];
+
   return (
     <div className="points-key">
-      <div>Royal Flush 5,000</div>
-      <div>Straight Flush 1,500</div>
-      <div>Four Of A Kind 600</div>
-      <div>Full House 300</div>
-      <div>Flush 200</div>
-      <div>Straight 125</div>
-      <div>Three Of A Kind 75</div>
-      <div>Two Pair 40</div>
-      <div>Jacks Or Better 10</div>
+      {points.map((point, index) => (
+        <div className="row" key={index}>
+          <div className="title">{point.title}</div>
+          <div className="value">
+            {point.value}
+            <img src={buxIcon} alt="bux icon" />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
